521e0a0bf9e457c7c400f46c3490cbba815794ca
Davidemanoel7 <davidemanoel34@gmail.com>
2024-02-06 20:01:22 -0300

    update object on response (201)

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -33,9 +33,7 @@ export async function getPoll( app: FastifyInstance ) {
             }
 
             return response.status(201).send({
-                id: poll.id,
-                title: poll.title,
-                updatedAt: poll.updatedAt
+                poll
             })
 
         } catch (error) {
