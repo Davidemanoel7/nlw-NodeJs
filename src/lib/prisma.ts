a5dacd170833f56b5c332f29499720eb7c2fafae
Davidemanoel7 <davidemanoel34@gmail.com>
2024-02-06 19:58:54 -0300

    add query to show SQL operation

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+    log: ['query'] //esse query vai mostrar a operação SQL executada quando uma requisição for enviada.
+});
