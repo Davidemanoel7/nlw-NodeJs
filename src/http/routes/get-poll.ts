1d86b179805bfbd0698e8c822e22af23066d1b6b
Davidemanoel7 <davidemanoel34@gmail.com>
2024-02-07 16:47:31 -0300

    update response and status code

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -32,7 +32,7 @@ export async function getPoll( app: FastifyInstance ) {
                 })
             }
 
-            return response.status(201).send({
+            return response.status(200).send({
                 poll
             })
 
