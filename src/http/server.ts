aa0b9857fc6622f2c6273caa50b16777c0508ba8
Davidemanoel7 <davidemanoel34@gmail.com>
2024-02-07 18:25:30 -0300

    add POST router to vote on poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,12 +1,19 @@
 import fastify from 'fastify';
-
+import cookie from '@fastify/cookie';
 import { createPoll } from './routes/post-poll';
 import { getPoll } from './routes/get-poll';
+import { voteOnPoll } from './routes/post-pollVote';
 
 const app = fastify();
 
+app.register(cookie, {
+    secret: 'polls-cookie-nlw',
+    hook:'onRequest'
+});
+
 app.register(createPoll);
 app.register(getPoll);
+app.register(voteOnPoll);
 
 app.listen({ port: 3333 })
     .then( () => {
